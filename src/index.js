6aa196fb31b8a1616ac166323f2dce52f40147f9
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-21 19:21:01 +0530

    changed to ES modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-const express=require('express');
-const connect=require('./config/database');
+import express from 'express';
+import { connect } from './config/database.js'
 const app=express();
 
+import service from './services/tweet-service.js';
 
     app.listen(3000,async ()=>{
         console.log("Server started");
         await connect();
         console.log("database connected");
-        
+        const ser =new service();
+        const tweet =await ser.create({
+            content:'#modified content'
+        });
+        console.log(tweet);
     });
