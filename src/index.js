9c47f902f457a92842d961c70d66dea213dd9ac8
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-22 16:17:23 +0530

    Added Controller and Rotes to create tweet api

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import { connect } from './config/database.js'
 const app=express();
 
+import bodyParser from 'body-parser';
+import apiRoutes from './routes/index.js';
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api',apiRoutes);
 
     app.listen(3000,async ()=>{
         console.log("Server started");
         await connect();
         console.log("database connected");
+        
         
     });
