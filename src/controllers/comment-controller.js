c40facece297bb3aad8355bb44354a1919fa1df5
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-27 22:22:55 +0530

    Added Comment creation on Tweets And on Comments also

---
+++ src/controllers/comment-controller.js
@@ -0,0 +1,23 @@
+import CommentService from '../services/comment-service.js';
+
+const commentService=new CommentService();
+
+export const createComment=async (req,res)=>{
+    try {
+        const response=await commentService.createComment(req.query.modelId,req.query.modelType,req.body.userId,req.body.content);
+        return res.status(200).json({
+            success:true,
+            data:response,
+            err:{},
+            message:"Succesfully created comment"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            data:{},
+            err:error,
+            message:"Something went wrong"
+        });
+    }
+} 
