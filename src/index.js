18404254ec047d6c877937b670bf83e88bc0d5d0
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-28 17:18:28 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const app=express();
 import bodyParser from 'body-parser';
 import apiRoutes from './routes/index.js';
 
+import passport from 'passport';
+import {passportAuth} from './config/jwt-middleware.js';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use('/api',apiRoutes);
 
