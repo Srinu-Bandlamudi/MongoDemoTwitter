365e26481d143523239128f978bc565ce6774a61
Srinu-Bandlamudi <srinubandlamudi55@gmail.com>
2024-02-28 12:19:07 +0530

    Added user signup with encrypted password

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ const app=express();
 import bodyParser from 'body-parser';
 import apiRoutes from './routes/index.js';
 
-import {TweetRepository,UserRepository} from './repository/index.js';
-import LikeService from './services/like-service.js';
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -16,13 +13,5 @@ app.use('/api',apiRoutes);
     app.listen(3007,async ()=>{
         console.log("Server started");
         await connect();
-        console.log("database connected");
-
-        // const userRepo=new UserRepository();
-        // const tweetRepo=new TweetRepository();
-        // const tweets=await tweetRepo.getAll(0,10);
-       
-        // const users=await userRepo.getAll(); 
-        // const likeService=new LikeService();
-        // await likeService.toggleLike(tweets[0].id,'Tweet',users[0].id);        
+        console.log("database connected"); 
     });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,8 @@ import { createTweet,getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
 
+import { signup } from '../../controllers/auth-controller.js';
+
 const router=express.Router();
 
 router.post('/tweets',createTweet);
@@ -12,5 +14,7 @@ router.post('/likes/toggle',toggleLike);
 router.post('/comments',createComment);
 
 router.get('/tweets/:id',getTweet);
+
+router.post('/signup',signup);
 
 export default router;
